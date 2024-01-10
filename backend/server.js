306d38978b12cdf8f25d39c839e43b2dde8f4754
mhmdnoman <mhmdnoman01@gmail.com>
2024-01-10 18:50:38 +0600

    added login and logout

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
+var cors = require("cors");
 const port = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
@@ -15,9 +16,32 @@ app.use(
     extended: false,
   })
 );
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+// // Cors setings
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+//   // res.setHeader(
+//   //   "Access-Control-Allow-Origin",
+//   //   "https://assingment-12-ph.netlify.app"
+//   // );
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.use(errorHandler);
 
